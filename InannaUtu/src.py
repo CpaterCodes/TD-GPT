52db0fbba490c29036aa6a8e7db987d66e5ad1cd
Charlie Paterson <Charlieed.Paterson@googlemail.com>
2023-05-27 11:07:02 +0100

    Tweak: Another attempt, but this time without context from test names. Next, should attempt to specify behaviour for a more complex unit of code

--- InannaUtu/src.py
+++ InannaUtu/src.py
@@ -1,12 +1,5 @@
 
 def inanna_utu(n: int) -> str:
-    """
-    A function that checks if a given number n is divisible by 3, 5, or both.
-    If n is divisible by 3, return "Inanna!"
-    If n is divisible by 5, return "Utu!"
-    If n is divisible by both 3 and 5, return "Inanna Utu!"
-    If n is not divisible by 3 or 5, return an empty string.
-    """
     if n % 3 == 0 and n % 5 == 0:
         return "Inanna Utu!"
     elif n % 3 == 0:
@@ -14,4 +7,4 @@ def inanna_utu(n: int) -> str:
     elif n % 5 == 0:
         return "Utu!"
     else:
-        return ""
+        return str(n)
--- InannaUtu/test.py
+++ InannaUtu/test.py
@@ -1,18 +1,23 @@
 import pytest
 from src import inanna_utu
 
+# Test names are anonymised: the test conditions alone will shape results!
 
-def test_Inanna_if_divisible_by_3():
+def test_a():
     for n in [3, 6, 9, 27, 96, 72, 57, 117]:
         assert inanna_utu(n) == "Inanna!"
 
 
-def test_Utu_if_divisible_by_5():
+def test_b():
     for n in [5, 10, 25, 40, 50, 70, 85, 100]:
         assert inanna_utu(n) == "Utu!"
 
 
-def test_Inanna_Utu_if_divisible_by_15():
+def test_c():
     for n in [15, 30, 45, 60, 75, 90, 105, 120, 135, 150]:
         assert inanna_utu(n) == "Inanna Utu!"
+
+def test_d():
+    for n in [2, 4, 8, 11, 13, 14, 16, 19, 31, 32, 71, 98]:
+        assert inanna_utu(n) == f"{n}"
 
